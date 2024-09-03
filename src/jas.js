eeae736e7aa8bbeb5fea334075710b997a12671d
tnvr-ahmd <thanveerahamed1100@gmail.com>
2024-09-03 10:42:21 +0530

    new version 3/9/2024

---
+++ src/jas.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './index.css';
+function Jas(){
+    return(
+    <div id="j">
+    <h1>JAswanthan</h1>
+
+        </div>
+)
+}
+export default Jas;
---
+++ src/next.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+
+function Next(){
+    return(
+        <>
+        <p>welcome</p>
+        </>
+    )
+}
+export default Next;
